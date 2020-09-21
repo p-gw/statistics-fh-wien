215d9b4ca41606004ff30d56602ddbecbd44ad67
Gewessler Philipp <Philipp.Gewessler@bmbwf.gv.at>
2020-09-21 16:46:48 +0200

    minor additions

--- notebooks/01_introduction.jl
+++ notebooks/01_introduction.jl
@@ -32,34 +32,44 @@ md"""
 # ╔═╡ 2e0c4ab0-f42b-11ea-1d7e-e18c5d37e4cf
 md"""
 ## About me
+
+Employed at the federal ministry for Education, Science & Research as a *psychometrician*. 
+
+> **Psychometrics** is a field of study concerned with the theory and technique of psychological measurement. [...]
+> 
+> The field is concerned with the objective *measurement* of skills and knowledge, abilities, attitudes, personality traits, and educational achievement.
+
+-- Wikipedia [🌐](https://en.wikipedia.org/wiki/Psychometrics)
+
+A main goal of my work is to ensure the *quality* of tasks (difficuly, fairness, ...) in the standardized Austrian matriculation exam by **empirical testing**.  
 """
 
 # ╔═╡ 57fbc3f0-f42b-11ea-3192-4d048c9b1208
 md"""
-## 💡 What are you going to learn? 
+## What are you going to learn? 
 ### Research design
 We will cover the basics of research- and questionnaire design. This includes,
 
 - The collection of quantitative data,
-- Scales & Tests,
+- Scales & tests,
 - Construction of questionnaires,
 - Sampling
 """
 
 # ╔═╡ cd4bcafa-f5be-11ea-3508-0d436b24bf33
 md"""
-## 💡 What are you going to learn? 
 ### Descriptive statistics
 
 Learn how to describe quantitative data *numerically* and *visually*.
 
-[insert COVID example here]
+[insert example here]
 """
 
 # ╔═╡ d5b2acb4-f5be-11ea-3392-f9d0aacc0e7e
 md"""
-## 💡 What are you going to learn? 
 ### Probability
+Get to know the basics of *probability theory* and *probability distributions*.
+
 What is $P(X \leq x)$?
 
 $(@bind x_value Slider(-3:0.01:3, default = -1))
@@ -84,54 +94,85 @@ begin
 	plot(pdf_plot, cdf_plot)
 end
 
+# ╔═╡ e75f6dae-fbfd-11ea-2b29-33b9c4082535
+md"""
+### Estimation
+
+The goal of statistical inference is to *estimate* some numerical quantity of a population from a sample. 
+
+"""
+
+# ╔═╡ f2f0daae-fbfd-11ea-06bc-edd8c65f2206
+dist = Normal()
+
+# ╔═╡ 4809d3d2-fbfe-11ea-1b1b-8f70440840cf
+plot(-3:0.01:3, pdf.(dist, -3:0.01:3), legend = false, xlimits = [-3, 3])
+
+# ╔═╡ 21ad28d0-fbff-11ea-014f-27606020265e
+begin
+	plusminus(x::Number, y::Number) = (x - y, x + y)
+	±(x, y) = plusminus(x, y)
+end
+
+# ╔═╡ 5ea86d30-fbff-11ea-1606-1d50b9d21e31
+function ci(x, α = 0.05)
+	est = mean(x)
+	se = std(x)/sqrt(length(x))
+	df = length(x) - 1
+	est ± se * quantile(TDist(df), 1 - α/2)
+end
+
+# ╔═╡ 020a7550-fc04-11ea-3049-3d7f4fc60023
+function sample_est(d::Distribution, n, α = 0.05)
+	s = rand(d, n)
+	mean(s), ci(s, α)
+end
+
+# ╔═╡ 58077af0-fc06-11ea-21cc-819b90a66812
+a, b = sample_est(Normal(0, 1), 10)
+
+# ╔═╡ c5eab620-fbfe-11ea-2b1f-29b691f642dd
+md"confidence level: $(@bind ci_level Slider(0.01:0.01:.99, default = 0.95))"
+
+# ╔═╡ 5b6879e0-fbfe-11ea-0273-238a3d2bea71
+md"sample size: $(@bind sample_size Slider(1:100, default = 10))"
+
+# ╔═╡ 71333df0-fbfe-11ea-037f-c12ae8aff59c
+sample = rand(dist, sample_size)
+
+# ╔═╡ 812545f0-fbfe-11ea-20c7-35831f0ad609
+histogram(sample, xlimits = [-3, 3])
+
+# ╔═╡ cffc2090-fc03-11ea-0285-b7570a8ae783
+begin
+	est = [sample_est(dist, sample_size, 1 - ci_level) for i = 1:50]
+	means = first.(est)
+	cis = last.(est)
+end
+
+# ╔═╡ 8f81f930-fc04-11ea-2084-b5e8759adaea
+begin
+	p = plot(means, 1:50, xlimits = [-0.5, 0.5], legend = false, seriestype = :scatter, color = "grey")
+	for i = 1:length(cis)
+		(x, y) = ([i, i], [first(cis[i]), last(cis[i])])
+		plot!(y, x, color = "grey", lw = 2)
+	end
+	p
+end
+
+# ╔═╡ 48e9cf60-fc05-11ea-3757-a51f87f5a87d
+100 - sum(@. first(cis) <= 0.0 <= last(cis)) 
+
+# ╔═╡ 5781be40-fc08-11ea-1314-eb4a9a21de77
+gr()
+
 # ╔═╡ dab70702-f5be-11ea-2201-7fbfffd42c20
 md"""
-## 💡 What are you going to learn? 
 ### Linear regression
 """
 
-# ╔═╡ ac41c918-f5c6-11ea-1ba3-a5a92d45b990
-n = 30
-
-# ╔═╡ 6383026c-f5c6-11ea-0bf3-b921b30c01f8
-x = rand(truncated(Normal(60, 20), 0, 100), n)
-
-# ╔═╡ 6bf2def6-f5c6-11ea-18ec-c7e7a1194767
-y = -5 .+ 0.83 .* x .+ rand(Normal(0, 8), n)
-
-# ╔═╡ 349992a4-f5c8-11ea-2e68-4f440a089704
-lm_data = DataFrame("x" => x,"y" => y)
-
-# ╔═╡ 63c76358-f5c8-11ea-17a8-71ff55e9841c
-ols_fit = lm(@formula(y ~ x), lm_data)
-
-# ╔═╡ ad490ec8-f5c8-11ea-2b3a-b19655e14edb
-α, β = coef(ols_fit)
-
-# ╔═╡ d79ea8f2-f5c8-11ea-1776-a9385d0efee5
-y_hat = predict(ols_fit)
-
-# ╔═╡ 319f9176-f5c9-11ea-219f-e713853fc37d
-resid = y - y_hat
-
-# ╔═╡ 7d3f577a-f5cb-11ea-134a-f5482f307912
-rect(w, h, x, y) = Shape(x .+ [0, w, w, 0], y .+ [0, 0, h, h])
-
-# ╔═╡ a08492ea-f5c6-11ea-2901-f349f6498e33
-begin
-	fig = plot(x, y, seriestype = :scatter, legend = false, xlimit = [0, 100], ylimit = [0, 100], aspect_ratio = :equal, xlabel = "test score (midterm)", ylabel = "test score (endterm)", markercolor = "grey", markerstrokecolor = "grey")
-	Plots.abline!(β, α, lw = 2)
-
-	for i = 1:n
-		plot!(rect(-resid[i], resid[i], x[i], y_hat[i]), opacity = 0.4, color = "grey")
-	end
-	
-	fig
-end
-
 # ╔═╡ f19592ea-f5be-11ea-3403-cbd6c9840a32
 md"""
-## 💡 What are you going to learn? 
 ### Testing
 """
 
@@ -143,8 +184,7 @@ md"""
 
 # ╔═╡ acc852c6-f5b7-11ea-2453-d1abcfbb6994
 md"""
-## How does this course work?
-### Homework 📘
+### 📘 Homework 
 - Total of **5 graded homeworks** (+1 ungraded) worth 8 points each,
 - Homeworks can be done either *alone* or *in pairs*.
 - Homeworks will be due until the upcoming synchronous distance learning session.
@@ -231,8 +271,17 @@ md"""
 This homework will **not** be graded!
 """
 
+# ╔═╡ be96b69e-fbfc-11ea-1248-f1ce5af6579f
+md"## Computational resources"
+
+# ╔═╡ c9464f22-fbfc-11ea-2e98-4dbae9055340
+md"This section can be safely ignored..."
+
 # ╔═╡ 1405cda6-f8ee-11ea-2f12-89130cedd15f
 center(x) = HTML("<div style='text-align: center'>$(Markdown.html(x))</div>")
+
+# ╔═╡ e31c67a0-fc0f-11ea-3541-6143fbce5289
+center(md"If you have any questions you can reach me at [philipp.gewessler@edu.fh-wien.ac.at](mailto:philipp.gewessler@edu.fh-wien.ac.at).")
 
 # ╔═╡ 65195ddc-f8ee-11ea-2902-0d0e3a14c7d0
 center(md"First possible date on *21.12.2020*!")
@@ -272,30 +321,76 @@ two_columns(md"""
 > See [syllabus](https://moodle.fh-wien.ac.at/moodle/mod/resource/view.php?id=514814) for schedule!
 """)
 
+# ╔═╡ ac41c918-f5c6-11ea-1ba3-a5a92d45b990
+n = 30
+
+# ╔═╡ 6383026c-f5c6-11ea-0bf3-b921b30c01f8
+x = rand(truncated(Normal(60, 20), 0, 100), n)
+
+# ╔═╡ 6bf2def6-f5c6-11ea-18ec-c7e7a1194767
+y = -5 .+ 0.83 .* x .+ rand(Normal(0, 8), n)
+
+# ╔═╡ 349992a4-f5c8-11ea-2e68-4f440a089704
+lm_data = DataFrame("x" => x,"y" => y)
+
+# ╔═╡ 63c76358-f5c8-11ea-17a8-71ff55e9841c
+ols_fit = lm(@formula(y ~ x), lm_data)
+
+# ╔═╡ ad490ec8-f5c8-11ea-2b3a-b19655e14edb
+α, β = coef(ols_fit)
+
+# ╔═╡ d79ea8f2-f5c8-11ea-1776-a9385d0efee5
+y_hat = predict(ols_fit)
+
+# ╔═╡ 319f9176-f5c9-11ea-219f-e713853fc37d
+resid = y - y_hat
+
+# ╔═╡ 7d3f577a-f5cb-11ea-134a-f5482f307912
+rect(w, h, x, y) = Shape(x .+ [0, w, w, 0], y .+ [0, 0, h, h])
+
+# ╔═╡ a08492ea-f5c6-11ea-2901-f349f6498e33
+begin
+	fig = plot(x, y, seriestype = :scatter, legend = false, xlimit = [0, 100], ylimit = [0, 100], aspect_ratio = :equal, xlabel = "test score (midterm)", ylabel = "test score (endterm)", markercolor = "grey", markerstrokecolor = "grey")
+	Plots.abline!(β, α, lw = 2)
+
+	for i = 1:n
+		plot!(rect(-resid[i], resid[i], x[i], y_hat[i]), opacity = 0.4, color = "grey")
+	end
+	
+	fig
+end
+
 # ╔═╡ Cell order:
-# ╠═39d4ca10-f427-11ea-2d6d-4392d7df0183
-# ╠═2e0c4ab0-f42b-11ea-1d7e-e18c5d37e4cf
+# ╟─39d4ca10-f427-11ea-2d6d-4392d7df0183
+# ╟─2e0c4ab0-f42b-11ea-1d7e-e18c5d37e4cf
+# ╟─e31c67a0-fc0f-11ea-3541-6143fbce5289
 # ╟─57fbc3f0-f42b-11ea-3192-4d048c9b1208
 # ╠═cd4bcafa-f5be-11ea-3508-0d436b24bf33
 # ╟─d5b2acb4-f5be-11ea-3392-f9d0aacc0e7e
 # ╟─0c7e1a42-f5d2-11ea-1d6f-b1a2cded18ba
 # ╟─bb7d81d0-f5ce-11ea-3873-4b14b1c50ec3
+# ╟─e75f6dae-fbfd-11ea-2b29-33b9c4082535
+# ╠═f2f0daae-fbfd-11ea-06bc-edd8c65f2206
+# ╠═4809d3d2-fbfe-11ea-1b1b-8f70440840cf
+# ╠═71333df0-fbfe-11ea-037f-c12ae8aff59c
+# ╠═812545f0-fbfe-11ea-20c7-35831f0ad609
+# ╠═21ad28d0-fbff-11ea-014f-27606020265e
+# ╠═5ea86d30-fbff-11ea-1606-1d50b9d21e31
+# ╠═020a7550-fc04-11ea-3049-3d7f4fc60023
+# ╠═58077af0-fc06-11ea-21cc-819b90a66812
+# ╠═cffc2090-fc03-11ea-0285-b7570a8ae783
+# ╠═c5eab620-fbfe-11ea-2b1f-29b691f642dd
+# ╠═5b6879e0-fbfe-11ea-0273-238a3d2bea71
+# ╠═8f81f930-fc04-11ea-2084-b5e8759adaea
+# ╠═48e9cf60-fc05-11ea-3757-a51f87f5a87d
+# ╟─5781be40-fc08-11ea-1314-eb4a9a21de77
 # ╟─dab70702-f5be-11ea-2201-7fbfffd42c20
 # ╟─a08492ea-f5c6-11ea-2901-f349f6498e33
 # ╟─503367b4-f5c6-11ea-0838-910afc85b4f3
-# ╟─ac41c918-f5c6-11ea-1ba3-a5a92d45b990
-# ╟─6383026c-f5c6-11ea-0bf3-b921b30c01f8
-# ╟─6bf2def6-f5c6-11ea-18ec-c7e7a1194767
-# ╟─349992a4-f5c8-11ea-2e68-4f440a089704
-# ╟─63c76358-f5c8-11ea-17a8-71ff55e9841c
-# ╟─ad490ec8-f5c8-11ea-2b3a-b19655e14edb
-# ╟─d79ea8f2-f5c8-11ea-1776-a9385d0efee5
-# ╟─319f9176-f5c9-11ea-219f-e713853fc37d
-# ╠═7d3f577a-f5cb-11ea-134a-f5482f307912
 # ╠═f19592ea-f5be-11ea-3403-cbd6c9840a32
 # ╟─6870907e-f42b-11ea-2c27-8b5bf45d06f3
 # ╟─409bd216-f8f1-11ea-2ecb-8188ab05d9ef
-# ╟─acc852c6-f5b7-11ea-2453-d1abcfbb6994
+# ╠═acc852c6-f5b7-11ea-2453-d1abcfbb6994
 # ╟─afd576fe-f5b7-11ea-11a4-654403973111
 # ╟─65195ddc-f8ee-11ea-2902-0d0e3a14c7d0
 # ╟─1e5060c8-f8ee-11ea-3532-69b3322d6045
@@ -303,7 +398,18 @@ two_columns(md"""
 # ╟─1e512524-f8ee-11ea-1501-5d62eea4f4d5
 # ╟─b32fc9fa-f8f0-11ea-008a-c3dacabaffbf
 # ╟─b2b6ef4e-f5b7-11ea-3346-e5d980ee7168
-# ╠═6f6412e6-f5bf-11ea-0495-2d22aeefc492
+# ╟─6f6412e6-f5bf-11ea-0495-2d22aeefc492
 # ╟─c4005d80-f5d2-11ea-1cf7-b50392525cb5
+# ╟─be96b69e-fbfc-11ea-1248-f1ce5af6579f
+# ╟─c9464f22-fbfc-11ea-2e98-4dbae9055340
 # ╠═1405cda6-f8ee-11ea-2f12-89130cedd15f
 # ╠═166dbb10-f8ee-11ea-0973-d511f7a6f02c
+# ╟─6383026c-f5c6-11ea-0bf3-b921b30c01f8
+# ╟─6bf2def6-f5c6-11ea-18ec-c7e7a1194767
+# ╟─349992a4-f5c8-11ea-2e68-4f440a089704
+# ╟─ad490ec8-f5c8-11ea-2b3a-b19655e14edb
+# ╟─63c76358-f5c8-11ea-17a8-71ff55e9841c
+# ╟─ac41c918-f5c6-11ea-1ba3-a5a92d45b990
+# ╟─d79ea8f2-f5c8-11ea-1776-a9385d0efee5
+# ╟─319f9176-f5c9-11ea-219f-e713853fc37d
+# ╠═7d3f577a-f5cb-11ea-134a-f5482f307912
--- notebooks/04_probabiliy_and_inference.jl
+++ notebooks/04_probabiliy_and_inference.jl
@@ -25,6 +25,12 @@ end
 # ╔═╡ c222f5a0-f430-11ea-288f-d394ab49704d
 md"# Probability & statistical inference"
 
+# ╔═╡ 24b48fe0-fc14-11ea-26c6-2b2dcabd228e
+md"## Set theory"
+
+# ╔═╡ 8b6fb1fe-fc14-11ea-33d4-f9a3e18de71b
+
+
 # ╔═╡ 61b63f50-f431-11ea-0864-637e5c1234db
 @bind μ Slider(-1:0.01:1, default = 0)
 
@@ -46,8 +52,20 @@ var(normDist)
 # ╔═╡ 0268b4a0-f432-11ea-3f2c-9fe01f6b1ec1
 plot(-5:0.01:5, pdf.(normDist, -5:0.01:5), ylimit = [0, 0.75], legend = false)
 
+# ╔═╡ 263f3210-fc15-11ea-1790-45dba8507246
+md"## Discrete probability distributions"
+
+# ╔═╡ 2b754580-fc15-11ea-38b3-a35d7aa65cc5
+md"""### Bernoulli distribution"""
+
+# ╔═╡ 37626710-fc15-11ea-3fe6-6d0a49881fb0
+md"""### Binomial distribution"""
+
+# ╔═╡ 3f0b0120-fc15-11ea-20a4-d76076d953a7
+md"""### Uniform distribution"""
+
 # ╔═╡ c883ff10-f431-11ea-3534-d7d48b4e2cf8
-md"## Poisson"
+md"### Poisson distribution"
 
 # ╔═╡ ce405980-f431-11ea-2f2d-1931d9d12580
 @bind λ Slider(0.5:0.1:100, default = 5)
@@ -64,11 +82,74 @@ var(poisDist)
 # ╔═╡ fbb947a0-f431-11ea-11d0-d928e9f7961e
 plot(0:50, pdf.(poisDist, 0:50), ylimit = [0, 0.65], xlabel = "x", ylabel = "P(X = x)", seriestype = :bar, legend = false)
 
+# ╔═╡ 6261c9b0-fc15-11ea-35d6-ed44c70cfde6
+md"## Continuous distributions"
+
+# ╔═╡ 93acf580-fc15-11ea-2147-11037c3ef3f5
+md"""### Exponential distribution"""
+
+# ╔═╡ 98b716f0-fc15-11ea-207a-95e605fd43b5
+md"""### Normal distribution"""
+
+# ╔═╡ 9b1aa96e-fc15-11ea-1a2e-e9a882e1ea5a
+md"""### Students T distribution"""
+
+# ╔═╡ bbd80270-fc15-11ea-365d-a9596c8fbdf3
+md"""### F distribution"""
+
+# ╔═╡ bd43c770-fc15-11ea-0280-fbca8eff4b1a
+md"""### χ² distribution"""
+
+# ╔═╡ dc5c2300-fc15-11ea-3fae-971e3ccc7e3d
+md"""## Inference & estimation"""
+
+# ╔═╡ 77bff3d0-fc16-11ea-0ed8-fb8e7cfba439
+md"""## Interpretations of probability"""
+
 # ╔═╡ 9e90ea0e-f436-11ea-0b7f-ed0033c1cd86
 plotly()
 
+# ╔═╡ 1899c410-fc13-11ea-3779-99e4db3939a1
+function two_columns(l, r, f = [50, 50])
+	html_l = Markdown.html(l)
+	html_r = Markdown.html(r)
+	res = """
+	<div style='display: flex'>
+		<div style='width: $(f[1])%'>$(html_l)</div>
+		<div style='width: $(f[2])%'>$(html_r)</div>	
+	</div>
+	"""
+	HTML(res)
+end
+
+# ╔═╡ e2f533c0-fc13-11ea-0921-5d974294b4fe
+two_columns(md"""
+""",
+md"""
+![asd](https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Sim%C3%A9onDenisPoisson.jpg/800px-Sim%C3%A9onDenisPoisson.jpg)
+	
+-- *Siméon Denis Poisson*
+""", [66, 33])
+
+# ╔═╡ 06490b90-fc13-11ea-190f-85fd09099588
+two_columns(md"""
+left content
+""", md"""
+![Carl Friedrich Gauß](https://upload.wikimedia.org/wikipedia/commons/3/33/Bendixen_-_Carl_Friedrich_Gau%C3%9F%2C_1828.jpg)
+
+-- *Carl Friedrich Gauss*
+""", [66, 33])
+
+# ╔═╡ 201b0000-fc13-11ea-325e-0fa70d049ec7
+center(x) = HTML("<div style='text-align: center'>$(Markdown.html(x))</div>")
+
+# ╔═╡ 209b538e-fc13-11ea-1a2a-f1766b3d5671
+
+
 # ╔═╡ Cell order:
-# ╠═c222f5a0-f430-11ea-288f-d394ab49704d
+# ╟─c222f5a0-f430-11ea-288f-d394ab49704d
+# ╠═24b48fe0-fc14-11ea-26c6-2b2dcabd228e
+# ╠═8b6fb1fe-fc14-11ea-33d4-f9a3e18de71b
 # ╠═61b63f50-f431-11ea-0864-637e5c1234db
 # ╠═7231265e-f431-11ea-3939-03cf49e56891
 # ╠═2e6d95d0-f431-11ea-0a3d-237a6205e52e
@@ -76,11 +157,28 @@ plotly()
 # ╠═5a27e400-f431-11ea-1a27-a1727caa79a4
 # ╠═a4e77af0-f431-11ea-012e-936cbd44c101
 # ╠═0268b4a0-f432-11ea-3f2c-9fe01f6b1ec1
+# ╠═263f3210-fc15-11ea-1790-45dba8507246
+# ╠═2b754580-fc15-11ea-38b3-a35d7aa65cc5
+# ╠═37626710-fc15-11ea-3fe6-6d0a49881fb0
+# ╠═3f0b0120-fc15-11ea-20a4-d76076d953a7
 # ╠═c883ff10-f431-11ea-3534-d7d48b4e2cf8
+# ╠═e2f533c0-fc13-11ea-0921-5d974294b4fe
 # ╠═ce405980-f431-11ea-2f2d-1931d9d12580
 # ╠═df7b0e20-f431-11ea-0304-2d1461c57a41
 # ╠═e81dbfa0-f431-11ea-0681-a1c7f11b3470
 # ╠═ec07cb10-f431-11ea-3146-59849abd0a5a
 # ╟─fbb947a0-f431-11ea-11d0-d928e9f7961e
+# ╠═6261c9b0-fc15-11ea-35d6-ed44c70cfde6
+# ╠═93acf580-fc15-11ea-2147-11037c3ef3f5
+# ╟─98b716f0-fc15-11ea-207a-95e605fd43b5
+# ╟─06490b90-fc13-11ea-190f-85fd09099588
+# ╠═9b1aa96e-fc15-11ea-1a2e-e9a882e1ea5a
+# ╠═bbd80270-fc15-11ea-365d-a9596c8fbdf3
+# ╟─bd43c770-fc15-11ea-0280-fbca8eff4b1a
+# ╠═dc5c2300-fc15-11ea-3fae-971e3ccc7e3d
+# ╠═77bff3d0-fc16-11ea-0ed8-fb8e7cfba439
 # ╠═9e90ea0e-f436-11ea-0b7f-ed0033c1cd86
 # ╠═138a0a50-f431-11ea-053a-8f430d193cb1
+# ╠═1899c410-fc13-11ea-3779-99e4db3939a1
+# ╠═201b0000-fc13-11ea-325e-0fa70d049ec7
+# ╠═209b538e-fc13-11ea-1a2a-f1766b3d5671
